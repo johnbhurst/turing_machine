efe082abe02143c2bffc84ef431f08d99bdc9c2b
John Hurst <john.b.hurst@gmail.com>
2016-05-05 09:48:26 +1200

    Initial project commit.

---
+++ src/main/java/nz/co/skepticalhumorist/turing/Main.java
@@ -0,0 +1,11 @@
+// Copyright 2016 John Hurst
+// John Hurst (john.b.hurst@gmail.com)
+// 2016-05-05
+
+package nz.co.skepticalhumorist.turing;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello.");
+  }
+}
